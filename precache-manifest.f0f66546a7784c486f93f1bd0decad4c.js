d0f90c84bbab1105017309aa797b0a9f084fdc61
Nazeervali531 <68688223+Nazeervali531@users.noreply.github.com>
2020-08-21 12:32:56 +0530

    Updates

---
+++ precache-manifest.f0f66546a7784c486f93f1bd0decad4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69061257fd0624007834727829dff05e",
+    "url": "/https//Nazeervali531.github.io/Netflix/index.html"
+  },
+  {
+    "revision": "58b854f24176e53437aa",
+    "url": "/https//Nazeervali531.github.io/Netflix/static/css/main.f8b54936.chunk.css"
+  },
+  {
+    "revision": "734f2645b764c9ef77cf",
+    "url": "/https//Nazeervali531.github.io/Netflix/static/js/2.7c295ea9.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/https//Nazeervali531.github.io/Netflix/static/js/2.7c295ea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58b854f24176e53437aa",
+    "url": "/https//Nazeervali531.github.io/Netflix/static/js/main.e670d487.chunk.js"
+  },
+  {
+    "revision": "b2b00d7fa5470e170e90",
+    "url": "/https//Nazeervali531.github.io/Netflix/static/js/runtime-main.ecb9f7c1.js"
+  }
+]);
